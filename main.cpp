430d8ac04ef4e966b5d90bc3a1b63738e4ac3227
Amber <78049937+arastella625@users.noreply.github.com>
2023-02-16 16:51:05 -0500

    Update main.cpp
    started to create read_file and debug

--- main.cpp
+++ main.cpp
@@ -3,12 +3,19 @@
 
 
 int main(int argc, char* argv[]){
-    Grade_Book gradebook;
+    
+    std::string file_name = argv[1];
+
+    Grade_book grades;
+
+    grades.read_file(file_name);
+    /*
     // GOAL: beat all of these test cases successfully
     gradebook.read_File("grades.txt");
     // TO-DO: implement run_task and make it a variadic method?
     gradebook.run_task("individual", "assignments", 4) // outputs the individual grade of assignment #4
     gradebook.run_task("category", "labs"); // outputs category total for labs
     gradebook.run_task("course"); // outputs the entire course grade
+    */
     return 0;
 }
