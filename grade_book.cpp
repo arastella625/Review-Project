cc63a2586efdece3744fa8d69519fccfbee3f49c
mikey <90413515+m-tedeschi@users.noreply.github.com>
2023-02-16 16:43:54 -0500

    Initial commit to ::course(), ::run_task()

--- grade_book.cpp
+++ grade_book.cpp
@@ -31,14 +31,29 @@ double Grade_Book::category(str task){
 return sum_grade
   std::cout << "total of the '" << task <<"is "<< sum_grade << "'.\n";
 
-  std::cout ," is: ", 
+  std::cout ," is: ",
+
 double Grade_Book::course(){
-  //M
-  return grade;
+	double assignments_grade;
+	assignments_grade = this->category(1); // fetch assignments weight
+	
+	double labs_grade;
+	labs_grade = this->category(2); // fetch labs weight
+	
+	double projects_grade;
+	projects_grade = this->category(3); // fetch projects weight
+	
+	// calculate overall course grade
+	// TO-DO: figure out what to replace 1000 with
+	double total_weight = (this->exam + assignments_grade + labs_grade + projects_grade) / 1000;
+	
+	std::cout << "Your final course grade, with the current inputs, is: " << total_weight << std::endl;
 }
   
 std::Grade_Book::run_task(){
-  
+  // TO-DO: implement variadic functionality?
+  // TO-DO: 'driver method' of Grade_Book class
+  // we will begin building this once we know that ::individual(), ::category(), and ::course() are all properly working
 }
   
   
