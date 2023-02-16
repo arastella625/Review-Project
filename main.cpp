921e9cc3e3537dfefd2e15ee84711086bc023906
m-tedeschi <90413515+m-tedeschi@users.noreply.github.com>
2023-02-16 15:58:41 -0500

    Added initial test cases (subject to change)
    Initial goal: write our methods such that these test cases pass successfully

--- main.cpp
+++ main.cpp
@@ -3,10 +3,12 @@
 
 
 int main(int argc, char* argv[]){
-
-
-
-
-
+    Grade_Book gradebook;
+    // GOAL: beat all of these test cases successfully
+    gradebook.read_File("grades.txt");
+    // TO-DO: implement run_task and make it a variadic method?
+    gradebook.run_task("individual", "assignments", 4) // outputs the individual grade of assignment #4
+    gradebook.run_task("category", "labs"); // outputs category total for labs
+    gradebook.run_task("course"); // outputs the entire course grade
     return 0;
 }
