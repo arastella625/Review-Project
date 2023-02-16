6014cd6015e30bb97b42872fceeb96295828f384
Amber <78049937+arastella625@users.noreply.github.com>
2023-02-16 16:52:05 -0500

    Update grade_book.h
    fixed header issues

--- grade_book.h
+++ grade_book.h
@@ -4,22 +4,25 @@
 #include <string>
 
 
-class Grade_Book {
+class Grade_book {
+
     private:
         std::vector<double> labs;
         std::vector<double> assignments;
         std::vector<double> projects;
         double exam;
         double user_total;
-        const double course_total;
-    
+        const double course_total = 1000;
+        
     public:
-        Grade_Book();
-        void read_File(std::string file_Name);
-        double individual();
-        double category();
+        Grade_book();
+        void read_file(std::string file_Name);
+        double individual(std::string task, int task_num);
+        double category(std::string task);
         double course();
         void run_task();
-}
+};
+
+
 
 #endif /* GRADE_BOOK */
