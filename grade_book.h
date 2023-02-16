5bc3a95ea456917b1af7a4bdad78708edf466f94
Giorgio72001 <123349711+Giorgio72001@users.noreply.github.com>
2023-02-16 14:46:25 -0500

    Update grade_book.h
    Initial commit.

--- grade_book.h
+++ grade_book.h
@@ -1,14 +1,28 @@
 #ifndef GRADE_BOOK
 #define GRADE_BOOK
+#include <vector>
+#include <string>
 
 
 class Grade_Book {
 
     private:
+    std::vector<double> labs;
+    std::vector<double> assignments;
+    std::vector<double> projects;
+    double exam;
+    double user_total;
+    const double course_total;
+    
 
 
 
     public:
+    Gradebook();
+    void read_File(std::string file_Name);
+    double individual();
+    double category();
+    double course();
 
 
 
