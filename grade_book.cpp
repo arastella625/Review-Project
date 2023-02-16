1ef77814d2052a5bc8462a8c2a92401f80dc05fb
Giorgio72001 <123349711+Giorgio72001@users.noreply.github.com>
2023-02-16 15:17:03 -0500

    Update grade_book.cpp
    Initial commit

--- grade_book.cpp
+++ grade_book.cpp
@@ -1,5 +1,33 @@
 #include "grade_book.h"
 #include <iostream>
+#include <vector>
+#include <string>
 
+Grade_Book::Gradebook(){
+  course_total = 1000;
+}
 
+void read_File(std::string file_Name){
+  //A
+}
 
+double Grade_Book::individual(int task, int task_num){
+  double grade;
+  
+  //If statements for four options
+  //Error Handling 
+  //G
+  
+  return grade;
+}
+
+void Grade_Book::category(int task){
+  //J
+  //add all tasks in specificcategory
+  //Returns all output
+}
+
+void Grade_Book::course(){
+  //M
+}
+  
