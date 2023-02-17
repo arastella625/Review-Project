936294bcc4b346c05e4404bb60c8c862c2085096
Amber <78049937+arastella625@users.noreply.github.com>
2023-02-17 14:48:40 -0500

    Update main.cpp
    added output variables and cout lines

--- main.cpp
+++ main.cpp
@@ -5,6 +5,15 @@
 
 int main(int argc, char* argv[]){
     
+     /*
+    // GOAL: beat all of these test cases successfully
+    gradebook.read_File("grades.txt");
+    // TO-DO: implement run_task and make it a variadic method?
+    gradebook.run_task("individual", "assignments", 4) // outputs the individual grade of assignment #4
+    gradebook.run_task("category", "labs"); // outputs category total for labs
+    gradebook.run_task("course"); // outputs the entire course grade
+    */
+    
     //read in file name from CLA
     std::string file_name = argv[1];
 
@@ -43,13 +52,58 @@ int main(int argc, char* argv[]){
         grades.run_task();
         */
     }
-    /*
-    // GOAL: beat all of these test cases successfully
-    gradebook.read_File("grades.txt");
-    // TO-DO: implement run_task and make it a variadic method?
-    gradebook.run_task("individual", "assignments", 4) // outputs the individual grade of assignment #4
-    gradebook.run_task("category", "labs"); // outputs category total for labs
-    gradebook.run_task("course"); // outputs the entire course grade
-    */
+
+    bool output = true;
+    std::string answer;
+    std::string task_answer;
+    int task_num_answer;
+    int task_grade;
+
+    while(output){
+
+        std::cout << "\n";
+
+        std::cout << "Does the user want to make changes to a grade? Please type either Y or N: ";
+
+        std::cin >> answer;
+
+        if(answer == "N"){
+
+            output = false;
+        }
+
+        std::cout << "What category will you be changing? Please copy one of these choices: " << "\n";
+        std::cout << "\n";
+        std::cout << "\t" << "labs" << "\n";
+        std::cout << "\t" << "assignments" << "\n";
+        std::cout << "\t" << "projects" << "\n";
+        std::cout << "\t" << "exam" << "\n";
+        std::cout << "\n" << "Category: ";
+
+        std::cin >> task_answer;
+
+        std::cout << "\n";
+
+        std::cout << "Which " << task_answer << " grade will you be changing?" << "\n" << "*Reminder - please use indexed numbers: ";
+
+        std::cin >> task_num_answer;
+
+        std::cout << "\n";
+
+        std::cout << "What will be the grade you are changing to?: ";
+
+        std::cin >> task_grade;
+
+        std::cout << "Changing grade now ... " << std::endl;
+
+        std::cout << "\n";
+
+        grades.output_file(file_name, task_answer, task_num_answer, task_grade);
+
+        std::cout << "Your grade has been changed! This program will now end." << "\n" << "Please re-run to use your changed grade." << std::endl;
+
+        output = false;
+
+    }
     return 0;
 }
