c6c8d3a8aa06598c32432ab09a97052685d0c417
Amber <78049937+arastella625@users.noreply.github.com>
2023-02-17 11:40:20 -0500

    Update main.cpp
    set up how code should work in main

--- main.cpp
+++ main.cpp
@@ -1,14 +1,48 @@
 #include "grade_book.h"
 #include <iostream>
+#include <string>
 
 
 int main(int argc, char* argv[]){
     
+    //read in file name from CLA
     std::string file_name = argv[1];
 
+    //create a Grade_book object
     Grade_book grades;
 
+    //read in the file to get the grades
     grades.read_file(file_name);
+
+    //if argc is 5 then store CLAs and call individual method 
+    //and output task method
+    if(argc == 5){
+
+        /*
+        std::string task = argv[3];
+        int task_num = std::stoi(argv[4]);
+        grades.individual(task, task_num);
+        grades.run_task();
+        */
+    }
+    //if argc is 3 then store CLAs and call category method
+    //and output task method
+    else if(argc == 3){
+
+        /*
+        std::string task = argv[2];
+        grades.category(task);
+        grades.run_task();
+        */
+    }
+    //else run the course and output task method
+    else{
+
+        /*
+        grades.course();
+        grades.run_task();
+        */
+    }
     /*
     // GOAL: beat all of these test cases successfully
     gradebook.read_File("grades.txt");
