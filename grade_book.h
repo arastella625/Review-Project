1fbc7858af26a53bc94b702f7cd1cb5d559fc349
Amber <78049937+arastella625@users.noreply.github.com>
2023-02-21 12:50:28 -0500

    fixed and got rid of some functions

--- grade_book.h
+++ grade_book.h
@@ -10,23 +10,17 @@ class Grade_book {
         std::vector<double> assignments;
         std::vector<double> projects;
         double exam;
-        double user_total;
         const double course_total = 1000;
         
     public:
         Grade_book();
-        std::vector<double> get_labs();
-        std::vector<double> get_assignments();
-        std::vector<double> get_projects();
         void read_file(std::string file_Name);
         void output_file(std::string file_name, std::string task_answer, int task_num, int task_grade);
         double individual(std::string task, int task_num);
-        //double category(std::string _category);
-        double Grade_book::category(int _category);
+        double category(std::string _category);
         double course();
         void print_individual();
         void print_category();
         void print_course();
-        //void run_task();
 };
 #endif /* GRADE_BOOK */
