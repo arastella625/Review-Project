05b24a4448832fee19e818c0b198d3bdbfdf107a
mikey <90413515+m-tedeschi@users.noreply.github.com>
2023-02-27 14:06:45 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,89 +1,17 @@
-#include "grade_book.h"
+/* @project Gradebook Review Project
+ * @desc A gradebook utilizing a points-achieved system to help track progress throughout CSC212
+ * @class URI CSC212
+ * @professor Jonathan Schrader
+ * @date 26 February 2023
+ * @group_members Amber Rastella, Giorgio Florentino, Junsheng Wu, Michael Tedeschi
+ */
+#include "gradebook.h"
 #include <iostream>
 #include <string>
 
-
 int main(int argc, char* argv[]){
-    //read in file name from CLA
-    // TO-DO: finish encapsulation of this section
-    std::string file_name = argv[1];
-    std::string category;
-    int cat_num;
+    Grade_Book grades; // instantiate a Grade_book object
+    grades.initialize(argc, argv); // call main driver method for Grade_book with CLA as parameters
 
-    //create a Grade_book object
-    Grade_book grades;
-
-    //read in the file to get the grades
-    grades.read_file(file_name);
-
-    //if argc is 5 then store CLAs and call individual method
-    //and output task method
-    if(argc == 5){
-
-        category = std::string(argv[3]);
-        cat_num = std::stoi(argv[4]);
-        grades.print_individual(category, cat_num);
-
-    }
-    //if argc is 3 then store CLAs and call category method
-    //and output task method
-    if(argc == 4){
-        category = std::string(argv[3]);
-        grades.print_category(category);
-    }
-    //else run the course and output task method
-    if (argc == 3) {
-        grades.print_course();
-    }
-
-    bool output = true;
-    char answer;
-    std::string task_answer;
-    int task_num_answer;
-    int task_grade;
-
-    while(output){
-
-        std::cout << "\n";
-
-        std::cout << "Does the user want to make changes to a grade? Please type either Y or N: " << std::endl;
-
-        std::cin >> answer;
-
-        if(answer == 'N'){
-            output = false;
-            break;
-        }
-
-        std::cout << "What category will you be changing? Please copy one of these choices: " << "\n";
-        std::cout << "\n";
-        std::cout << "\t" << "labs" << "\n";
-        std::cout << "\t" << "assignments" << "\n";
-        std::cout << "\t" << "projects" << "\n";
-        std::cout << "\t" << "exam" << "\n";
-        std::cout << "\n" << "Category: ";
-
-        std::cin >> task_answer;
-
-        std::cout << "\n";
-
-        std::cout << "Which " << task_answer << " grade will you be changing?" << "\n";
-
-        std::cin >> task_num_answer;
-
-        std::cout << "\n";
-
-        std::cout << "What will be the grade you are changing to?: ";
-
-        std::cin >> task_grade;
-
-        std::cout << "Changing grade now ... " << std::endl;
-
-        std::cout << "\n";
-
-        grades.output_file(file_name, task_answer, task_num_answer, task_grade);
-
-        std::cout << "Your grade has been changed!" << std::endl;
-    }
     return 0;
 }
